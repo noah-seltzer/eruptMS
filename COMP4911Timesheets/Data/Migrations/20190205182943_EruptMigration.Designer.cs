5690c0856db8496454f5d9b141b8f4a08bf89373
yang <ytong13@my.bcit.ca>
2019-02-08 09:27:58 -0800

    add Utility class (HashEncrypt and HashDecrypt method)

--- COMP4911Timesheets/Data/Migrations/20190205182943_EruptMigration.Designer.cs
+++ COMP4911Timesheets/Data/Migrations/20190205182943_EruptMigration.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace COMP4911Timesheets.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20190204230716_EruptMigration")]
+    [Migration("20190205182943_EruptMigration")]
     partial class EruptMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.1-servicing-10028")
+                .HasAnnotation("ProductVersion", "2.2.0-rtm-35687")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
