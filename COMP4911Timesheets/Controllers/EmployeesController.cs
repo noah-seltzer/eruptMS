57f55a7cde77bd5550ca12f1946160ac10be859a
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-02-13 14:22:19 -0800

    Creating new employees registers them as users

---
+++ COMP4911Timesheets/Controllers/EmployeesController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP4911Timesheets.Data;
+using COMP4911Timesheets.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace COMP4911Timesheets.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Employee> _userManager;
+
+        public EmployeesController(
+            ApplicationDbContext context,
+            UserManager<Employee> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Employees.Include(e => e.Approver).Include(e => e.Supervisor);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Approver)
+                .Include(e => e.Supervisor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["ApproverId"] = new SelectList(_context.Approvers, "ApproverId", "ApproverId");
+            ViewData["SupervisorId"] = new SelectList(_context.Supervisors, "SupervisorId", "SupervisorId");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeId,FirstName,LastName,Title,CreatedTime,FlexTime,VacationTime,Status,ApproverId,SupervisorId,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                //_context.Add(employee);
+                
+
+                //var user = new Employee { UserName = Input.Email, Email = Input.Email };
+                await _userManager.CreateAsync(employee,"P@ssw0rd");
+                //if (result.Succeeded)
+                //{
+                //    var code = await _userManager.GenerateEmailConfirmationTokenAsync(employee);
+                //    var callbackUrl = Url.Page(
+                //        "/Account/ConfirmEmail",
+                //        pageHandler: null,
+                //        values: new { userId = employee.Id, code },
+                //        protocol: Request.Scheme);
+
+                //    await _signInManager.SignInAsync(employee, isPersistent: false);
+                //    //return LocalRedirect(returnUrl);
+                //}
+                //foreach (var error in result.Errors)
+                //{
+                //    ModelState.AddModelError(string.Empty, error.Description);
+                //}
+                //await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApproverId"] = new SelectList(_context.Approvers, "ApproverId", "ApproverId", employee.ApproverId);
+            ViewData["SupervisorId"] = new SelectList(_context.Supervisors, "SupervisorId", "SupervisorId", employee.SupervisorId);
+            
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApproverId"] = new SelectList(_context.Approvers, "ApproverId", "ApproverId", employee.ApproverId);
+            ViewData["SupervisorId"] = new SelectList(_context.Supervisors, "SupervisorId", "SupervisorId", employee.SupervisorId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("EmployeeId,FirstName,LastName,Title,CreatedTime,FlexTime,VacationTime,Status,ApproverId,SupervisorId,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApproverId"] = new SelectList(_context.Approvers, "ApproverId", "ApproverId", employee.ApproverId);
+            ViewData["SupervisorId"] = new SelectList(_context.Supervisors, "SupervisorId", "SupervisorId", employee.SupervisorId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Approver)
+                .Include(e => e.Supervisor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(string id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
--- COMP4911Timesheets/Models/Employee.cs
+++ COMP4911Timesheets/Models/Employee.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace COMP4911Timesheets.Models
 {
-    public class Employee : IdentityUser
+    public class Employee : IdentityUser <string>
     {
         public Employee() : base() { }
 
