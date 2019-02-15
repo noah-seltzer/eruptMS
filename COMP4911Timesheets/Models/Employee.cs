d5dba3bbaffb5567e183ddb0b9acd961c4508e94
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-02-15 08:30:38 -0800

    Edit columns shown for employee create/details/index

--- COMP4911Timesheets/Models/Employee.cs
+++ COMP4911Timesheets/Models/Employee.cs
@@ -6,18 +6,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace COMP4911Timesheets.Models
 {
-    public class Employee : IdentityUser <string>
+    public class Employee : IdentityUser<string>
     {
         public Employee() : base() { }
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Employee ID")]
         public int EmployeeId { get; set; }
         //public string Email { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public int Title { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
         public DateTime CreatedTime { get; set; }
+
         public double FlexTime { get; set; }
         public double VacationTime { get; set; }
         public int Status { get; set; }
@@ -34,5 +43,33 @@ namespace COMP4911Timesheets.Models
 
         public int? SupervisorId { get; set; }
         public Supervisor Supervisor { get; set; }
+
+        public readonly Dictionary<int, string> jobTitles = new Dictionary<int, string>
+        {
+            {10, "Administrator"},
+            {100, "HR Manager"},
+            {200, "Line Manager"},
+            {210, "Supervisor"},
+            {510, "Business Analyst"},
+            {520, "Technical Writer"},
+            {610, "Software Architect"},
+            {620, "Software Developer"},
+            {621, "Senior Software Developer"},
+            {622, "Junior Software Developer"},
+            {630, "Software Tester"},
+            {710, "UI Designer"}
+        };
+        
+        public string StringTitle(int key)
+        {
+            string result = null;
+
+            if (jobTitles.ContainsKey(key))
+            {
+                result = jobTitles[key];
+            }
+
+            return result;
+        }
     }
 }
