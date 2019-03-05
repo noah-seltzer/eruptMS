428c1fd11e081894d0e3f1551625bf5f75a11797
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-03-05 15:52:16 -0800

    Change user profile page to show account details

--- COMP4911Timesheets/Models/ApplicationRole.cs
+++ COMP4911Timesheets/Models/ApplicationRole.cs
@@ -9,14 +9,14 @@ namespace COMP4911Timesheets.Models
     public class ApplicationRole : IdentityRole
     {
 
-        public static readonly string AD = "AD";
-        public static readonly string TA = "TA";
-        public static readonly string HR = "HR";
-        public static readonly string PM = "PM";
-        public static readonly string PA = "PA";
-        public static readonly string LM = "LM";
-        public static readonly string RE = "RE";
-        public static readonly string EM = "EM";
+        public static readonly string AD = "AD"; // Admin
+        public static readonly string TA = "TA"; // Timesheet Approver
+        public static readonly string HR = "HR"; // Human Resources
+        public static readonly string PM = "PM"; // Project Manager
+        public static readonly string PA = "PA"; // Project Assistant
+        public static readonly string LM = "LM"; // Line Manager
+        public static readonly string RE = "RE"; // Responsible Engineer
+        public static readonly string EM = "EM"; // Employee
 
         public ApplicationRole() : base() { }
 
