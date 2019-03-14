015d2d37b88067f0a3b649a439c4603dc7d5e576
Daniel DiIorio <ddiiorio@my.bcit.ca>
2019-03-13 19:12:47 -0700

    Add view helper class so active controller/section is highlighted in navbar

---
+++ COMP4911Timesheets/ViewModels/NavViewHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP4911Timesheets.ViewModels
+{
+    public static class NavViewHelper
+    {
+        public static string IsSelected(this IHtmlHelper htmlHelper, string controllers, string actions, string cssClass = "active")
+        {
+            string currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
+            string currentController = htmlHelper.ViewContext.RouteData.Values["controller"] as string;
+
+            IEnumerable<string> acceptedActions = (actions ?? currentAction).Split(',');
+            IEnumerable<string> acceptedControllers = (controllers ?? currentController).Split(',');
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
+                cssClass : String.Empty;
+        }
+    }
+}
