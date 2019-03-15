824626595f2384f63d5e1aa7e3a48b5ef2023f06
apollo78124 <98apollo@naver.com>
2019-03-15 15:45:46 -0700

    Time sheet approval see how many is waiting to be approved

---
+++ COMP4911Timesheets/Controllers/ApproveController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMP4911Timesheets.Data;
+using COMP4911Timesheets.Models;
+
+namespace COMP4911Timesheets.Controllers
+{
+    public class ApproveController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApproveController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Approve
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Employees.Include(e => e.Approver).Include(e => e.Supervisor);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Approve/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Approver)
+                .Include(e => e.Supervisor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Approve/Create
+        public IActionResult Create()
+        {
+            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id");
+            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id");
+            return View();
+        }
+
+        // POST: Approve/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeId,Email,FirstName,LastName,Title,CreatedTime,FlexTime,VacationTime,Status,ApproverId,SupervisorId,Id,UserName,NormalizedUserName,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id", employee.ApproverId);
+            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id", employee.SupervisorId);
+            return View(employee);
+        }
+
+        // GET: Approve/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id", employee.ApproverId);
+            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id", employee.SupervisorId);
+            return View(employee);
+        }
+
+        // POST: Approve/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("EmployeeId,Email,FirstName,LastName,Title,CreatedTime,FlexTime,VacationTime,Status,ApproverId,SupervisorId,Id,UserName,NormalizedUserName,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id", employee.ApproverId);
+            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id", employee.SupervisorId);
+            return View(employee);
+        }
+
+        // GET: Approve/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employees
+                .Include(e => e.Approver)
+                .Include(e => e.Supervisor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Approve/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(string id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
