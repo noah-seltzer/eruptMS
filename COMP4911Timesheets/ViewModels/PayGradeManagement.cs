e2fc86e3e01a23e783d124ac6a55663f0829d6fb
Edmund Ham <hamsu2959@gmail.com>
2019-03-19 13:39:09 -0700

    added Plevel controller

---
+++ COMP4911Timesheets/ViewModels/PayGradeManagement.cs
@@ -0,0 +1,10 @@
+using COMP4911Timesheets.Models;
+
+namespace COMP4911Timesheets.ViewModels
+{
+    public class PayGradeManagement
+    {
+        public EmployeePay EmployeePay { get; set; }
+        public PayGrade PayGrade { get; set; }
+    }
+}
