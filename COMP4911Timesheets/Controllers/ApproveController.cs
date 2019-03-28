6b8e043536abd9382d98adb62995b56572677322
apollo78124 <98apollo@naver.com>
2019-03-28 14:02:48 -0700

    minor code cleaning

--- COMP4911Timesheets/Controllers/ApproveController.cs
+++ COMP4911Timesheets/Controllers/ApproveController.cs
@@ -22,8 +22,8 @@ namespace COMP4911Timesheets.Controllers
         // GET: Approve
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Employees.Include(e => e.Approver).Include(e => e.Supervisor).Include(e => e.Timesheets);
-            return View(await applicationDbContext.ToListAsync());
+            var Employees = _context.Employees.Include(e => e.Approver).Include(e => e.Supervisor).Include(e => e.Timesheets);
+            return View(await Employees.ToListAsync());
         }
 
         // GET: Approve/Details/5
@@ -46,114 +46,8 @@ namespace COMP4911Timesheets.Controllers
 
             return View(employee);
         }
-
-        // GET: Approve/Create
-        public IActionResult Create()
-        {
-            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id");
-            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id");
-            return View();
-        }
-
-        // POST: Approve/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("EmployeeId,Email,FirstName,LastName,Title,CreatedTime,FlexTime,VacationTime,Status,ApproverId,SupervisorId,Id,UserName,NormalizedUserName,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] Employee employee)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(employee);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id", employee.ApproverId);
-            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id", employee.SupervisorId);
-            return View(employee);
-        }
-
-        // GET: Approve/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var employee = await _context.Employees.FindAsync(id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id", employee.ApproverId);
-            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id", employee.SupervisorId);
-            return View(employee);
-        }
-
-        // POST: Approve/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        /**
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("EmployeeId,Email,FirstName,LastName,Title,CreatedTime,FlexTime,VacationTime,Status,ApproverId,SupervisorId,Id,UserName,NormalizedUserName,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] Employee employee)
-        {
-            if (id != employee.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(employee);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmployeeExists(employee.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ApproverId"] = new SelectList(_context.Employees, "Id", "Id", employee.ApproverId);
-            ViewData["SupervisorId"] = new SelectList(_context.Employees, "Id", "Id", employee.SupervisorId);
-            return View(employee);
-        }
-            */
-
-
-
-        // GET: Approve/Delete/5
-        /**
-        public async Task<IActionResult> Approve(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            int x = Int32.Parse(id);
-
-            var timesheet = await _context.Timesheets
-                .Include(e => e.TimesheetRows)
-                .FirstOrDefaultAsync(m => m.TimesheetId == x);
-            if (timesheet == null)
-            {
-                return NotFound();
-            }
-
-            return View(timesheet);
-        }
-        */
+        
+        
         // POST: Approve/Delete/5
         public async Task<IActionResult> TimesheetView(int id)
         {
