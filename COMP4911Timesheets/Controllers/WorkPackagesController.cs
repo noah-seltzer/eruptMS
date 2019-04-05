090187219639d6e83d9e260edc6551545e4d943b
yang <ytong13@my.bcit.ca>
2019-04-05 10:34:31 -0700

    fix appsetting

--- COMP4911Timesheets/Controllers/WorkPackagesController.cs
+++ COMP4911Timesheets/Controllers/WorkPackagesController.cs
@@ -343,6 +343,7 @@ namespace COMP4911Timesheets.Controllers
             var projectEmployee = _context.ProjectEmployees
                 .Where(u => u.ProjectId == id && u.EmployeeId == users.Id).FirstOrDefault();
 
+
             if ((User.IsInRole(role: "PM") || User.IsInRole(role: "PA")) && projectEmployee == null)
             {
                 TempData["info"] = "You are not the project's PM or PA, Please choose the currect project";
